037ebaa0579100b04c393f06fa05f64903dd3767
Miguel Angel <leonaguirrem3@gmail.com>
2019-10-21 21:12:55 -0500

    Docs: Se colocaron comentarios para saber como se realizó el codigo y su funcionamiento.

--- Suma_Producto_Vectores.py
+++ Suma_Producto_Vectores.py
@@ -7,6 +7,12 @@ class node:
 # Creamos la clase linked_list
 class linked_list:
 
+    """
+    Se asignan los atributos, los cuales son
+    las operaciones que se desean obtener, en la multiplicacion
+    toma un valor de 1 ya que si le asignamos un valor de 0, la
+    multilpicacion dara como resultado 0.
+    """
     __sumatoria = 0
     __multiplicacion = 1
 
@@ -61,9 +67,14 @@ class linked_list:
     def print_list(self):
         node = self.head
         while node != None:
-            print(node.data, node.next)
+            print(node.data, node.next)  #Se muestra el dato y el nodo siguiente
             node = node.next
 
+    """
+    Se agrega el metodo para obtener las sumas
+    de los nodos, en el que se hace us de un ciclo while, en el que 
+    realiza un recorrido hasta que el nodo sea nulo finaliza.
+    """
     def obtenerSumatoria(self):
         node = self.head
 
@@ -73,6 +84,10 @@ class linked_list:
 
         return self.__sumatoria
 
+    """
+    Se asigna el metodo para realizar la multiplicacion de los 
+    nodos.
+    """
     def obtenerMultiplicacion(self):
         node = self.head
 
@@ -83,26 +98,39 @@ class linked_list:
         return self.__multiplicacion
 
 s = linked_list()  # Instancia de la clase
-s.add_at_front(5)  # Agregamos un elemento al frente del nodo
-s.add_at_end(8)  # Agregamos un elemento al final del nodo
-s.add_at_front(9)  # Agregamos otro elemento al frente del nodo
+x = linked_list()
+
+"""
+Datos que se le agregan a lo nodos en el Vector 1:
+"""
+# Se agregan elementos al frente del nodo
+s.add_at_front(5)
+s.add_at_front(9)
+
+# Se agregan elementos al final del nodo
 s.add_at_end(4)
 s.add_at_end(12)
+s.add_at_end(8)
 
-x = linked_list()
-x.add_at_front(14)  # Agregamos un elemento al frente del nodo
-x.add_at_end(11)  # Agregamos un elemento al final del nodo
-x.add_at_front(7)  # Agregamos otro elemento al frente del nodo
-x.add_at_end(3)
-x.add_at_end(8)
+"""
+Datos que se le agregan a lo nodos en el Vector 2:
+"""
+# Se agregan elementos al frente del nodo
+x.add_at_front(7)
+x.add_at_front(12)
+
+# Se agregan elementos al final del nodo
+x.add_at_end(15)
+x.add_at_end(6)
+x.add_at_end(2)
 
 print("Vector 1:")
-s.print_list()  # Imprimimos la lista de nodos
+s.print_list()  # Se imprime la lista de nodos del vector 1
 print("----------------------------------------------------------")
 print("Vector 2:")
-x.print_list()
+x.print_list()   # Se imprime la lista de nodos del vector 2
 
 print()
 print("**********************************************************")
-print("La suma de los vectores es: ", int(s.obtenerSumatoria() + x.obtenerSumatoria()))
-print("El producto escalar es: ", int(s.obtenerMultiplicacion() + x.obtenerMultiplicacion()))
+print("La suma de los vectores es: ", int(s.obtenerSumatoria() + x.obtenerSumatoria()))  # Se muestra la suma de los dos vectores
+print("El producto escalar es: ", int(s.obtenerMultiplicacion() + x.obtenerMultiplicacion()))  # Se muestra la multiplicacion de los dos vectores
